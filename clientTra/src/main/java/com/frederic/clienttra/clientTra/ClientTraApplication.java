50240723f5bc28acbac84531c8ba204bff908295
FredericJaquet <frederic.jaquet@gmail.com>
2025-06-18 20:56:07 +0200

    DataBase_Fiest_Definition

---
+++ clientTra/src/main/java/com/frederic/clienttra/clientTra/ClientTraApplication.java
@@ -0,0 +1,13 @@
+package com.frederic.clienttra.clientTra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientTraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientTraApplication.class, args);
+	}
+
+}
