722eea75f9feef2428a44de60ca5e344b93ec40f
FredericJaquet <frederic.jaquet@gmail.com>
2025-07-19 10:41:46 +0200

    Fix: InternationalizationConfig

--- clientTra/src/main/java/com/frederic/clienttra/config/InternationalizationConfig.java
+++ clientTra/src/main/java/com/frederic/clienttra/config/InternationalizationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class InternationalizationConfig {
@@ -14,6 +15,13 @@ public class InternationalizationConfig {
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setFallbackToSystemLocale(false);
         return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        factoryBean.setValidationMessageSource(messageSource);
+        return factoryBean;
     }
 
 }
